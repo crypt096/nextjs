5234711def35c93e9280d297506fe1004ed4d343
crypt096 <acvjetan@gmail.com>
2021-01-26 11:33:26 +0100

    Adding other components.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Navbar from "../components/Navbar";
+import Footer from '../components/Footer';
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <Footer />
     </div>
   )
 }
