698fb719dc6ddbc4e37e61feec2ecc23c25e2ab2
crypt096 <acvjetan@gmail.com>
2021-01-26 11:27:45 +0100

    Add pages & routes.

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
+    </div>
+  );
+}
+
+export default About;
