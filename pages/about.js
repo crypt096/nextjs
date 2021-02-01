588b38c75e6e28cd445fd651b9ef7fd8f3b88c7e
crypt096 <acvjetan@gmail.com>
2021-02-01 11:58:44 +0100

    Add metadata.

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed alias quaerat temporibus aperiam assumenda nihil, dolores officia eos atque qui minima, quasi veritatis? Dolorem vel similique animi reprehenderit. Magnam ipsum inventore dicta animi, odit maxime unde ut et, possimus at sed quod deleniti impedit reprehenderit? Veniam iure consequuntur aliquam officiis?</p>
+      </div>
+    </>
   );
 }
 
